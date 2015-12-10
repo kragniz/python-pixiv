9b6f762b02997ae412d7a22e7846e087cadf0708
Louis Taylor <louis@kragniz.eu>
2015-12-10 15:32:51 +0000

    Add base package

---
+++ pixiv/__init__.py
@@ -0,0 +1,3 @@
+__author__ = 'Louis Taylor'
+__email__ = 'louis@kragniz.eu'
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+requirements = [
+    # TODO: put package requirements here
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='pixiv',
+    version='0.1.0',
+    description="Pixiv API client.",
+    long_description=readme + '\n\n' + history,
+    author="Louis Taylor",
+    author_email='louis@kragniz.eu',
+    url='https://github.com/kragniz/pixiv',
+    packages=[
+        'pixiv',
+    ],
+    package_dir={'pixiv':
+                 'pixiv'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="LGPLv3",
+    zip_safe=False,
+    keywords='pixiv',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
