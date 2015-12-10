bc9b39c12119dfa19c3d3fca9eb608b0294053a2
Louis Taylor <louis@kragniz.eu>
2015-12-10 17:42:47 +0000

    Add dummy test

---
+++ tests/test_pixiv.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+'''
+test_pixiv
+----------
+
+Tests for `pixiv` module.
+'''
+
+import unittest
+
+from pixiv import pixiv
+
+
+class TestPixiv(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_something(self):
+        pass
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(unittest.main())
