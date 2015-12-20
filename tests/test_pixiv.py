0b86e92706fd35e660416fb16a02821d992b84c8
Louis Taylor <louis@kragniz.eu>
2015-12-20 12:22:12 +0000

    Add recorded login

--- tests/test_pixiv.py
+++ tests/test_pixiv.py
@@ -1,5 +1,6 @@
 '''Tests for `pixiv` module.'''
 
+import os
 import unittest
 
 import betamax
@@ -10,13 +11,14 @@ import pixiv
 
 pytest.mark.usefixtures('betamax_session')
 
-test_username = 'username'
-test_password = 'password'
+# run tests with $ USERNAME=live-username PASSWORD=live-password tox -e py27
+test_username = os.environ.get('USERNAME', 'pixiv-username')
+test_password = os.environ.get('PASSWORD', 'pixiv-password')
 
 with betamax.Betamax.configure() as config:
     config.cassette_library_dir = 'tests/cassettes'
-    config.define_cassette_placeholder('pixiv-password', test_password)
     config.define_cassette_placeholder('pixiv-username', test_username)
+    config.define_cassette_placeholder('pixiv-password', test_password)
 
 
 class TestPixiv:
